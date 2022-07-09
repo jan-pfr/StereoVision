83340bfa8a36eac29d88836038b96aa94afd3c24
Jan Pfeiffer <jan.pfeiffer@hs-furtwangen.de>
2022-07-10 00:50:44 +0200

    massive refactoring. added requirements for conda. added config features.

--- StereoVision/core.py
+++ StereoVision/core.py
@@ -1,5 +1,5 @@
 import logging
-
+import configparser
 import cv2 as cv
 import numpy as np
 import time
@@ -10,185 +10,211 @@ from counter import CountsPerSec
 from cameraCapture import CameraCapture
 from trajectoryPrediction import TrajectoryPrediction
 
-# Open both cameras in extra Threads
-capLeft = CameraCapture(0).start()
-capRight = CameraCapture(1).start()
 
-# toDo: Parameter that have to be moved out of the Code into a config-File
-
-# Amount of minimum Samples, before a trajectroy is tried to be predicted
-min_samples = 2
-min_time_diff = float(0.04)
-
-# Stereo vision setup parameters
-frame_rate = 30  # Camera frame rate (maximum at 120 fps)
-B = 18.1  # Distance between the cameras [cm]
-f = 1  # Camera lense's focal length [mm]
-alpha = 60  # Camera field of view in the horizontal plane [degrees]
-
-# HSV Range for a Tennis Ball -> Denpends on light-conditions
-lowerHSVRange = (26, 134, 28)
-higherHSVRange = (43, 255, 255)
-
-# Array for coordinates of the object, if one is detected in the picture
-positions = []
-
-# Empty Arrays for old frames
-left_frame_old = np.array([])
-right_frame_old = np.array([])
-
-# counter for keeping track of the amount of coordinates so far.
-count = 0
-
-# Logger configuration
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
-
-# Create Objects from the TrajectoryPredictor and others
-# toDo: better Object Names
-tp = TrajectoryPrediction(min_samples)
-tr = Triangulation()
-fltr = Filter(lowerHSVRange, higherHSVRange)
-od = ObjectDetection()
-
-# allow Camera warm up
-time.sleep(2.0)
+def parse_int_tuple(s: str):
+    return tuple(int(i.strip()) for i in s[1:-1].split(','))
 
 
-### Functions that are used inside this application
+class Application:
 
-def put_iterations_per_sec(frame, iterations_per_sec):
-    """
-    Put the Iterations per Second Number on to a frame.
+    def __init__(self):
+        """
+        Import all necessary params and initiate variables.
 
-    :param frame: as np.array
-    :param iterations_per_sec: number
-    :return: frame with number
-    """
+        """
 
-    cv.putText(frame, "{:.0f} iterations/sec".format(iterations_per_sec),
-               (10, 650), cv.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255))
-    return frame
+        # Configure parser so it is able to parse tuples
+        self.config = configparser.ConfigParser(converters={'tuple': parse_int_tuple})
+        self.config.read('config.ini')
+
+        # Import settings, fallbacks if the import fails
+
+        # Trajectory Settings
+        self.min_samples = self.config['TrajectoryPredictionSettings'].getint('minSamples', fallback=2)
+        self.min_time_diff = self.config['TrajectoryPredictionSettings'].getfloat('minTimeDifference', fallback=0.04)
+
+        # Camera settings
+        self.baseline = self.config['CameraSettings'].getfloat('baseline', fallback=18.1)
+        self.focal_length = self.config['CameraSettings'].getint('focalLength', fallback=3)
+        self.fov = self.config['CameraSettings'].getint('fieldOfView', fallback=60)
+        self.left_id = self.config['CameraSettings'].getint('leftID', fallback=0)
+        self.right_id = self.config['CameraSettings'].getint('rightID', fallback=1)
+
+        # HSV ranges
+        self.low_hsv_range = self.config['HSVRange'].gettuple('lowHSVRange')
+        self.high_hsv_range = self.config['HSVRange'].gettuple('highHSVRange')
+
+        # Start Cameras
+        self.capLeft = CameraCapture(self.left_id).start()
+        self.capRight = CameraCapture(self.right_id).start()
+        time.sleep(2)  # warm up
+
+        # Logger configuration
+        logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+
+        # Initialize helper objects
+        self.tp = TrajectoryPrediction(self.min_samples)
+        self.tr = Triangulation()
+        self.fltr = Filter(self.low_hsv_range, self.high_hsv_range)
+        self.od = ObjectDetection()
+
+        # Array for coordinates of the object, if one is detected in the picture
+        self.positions = []
+
+        # Empty Arrays for old frames
+        self.left_frame_old = np.array([])
+        self.right_frame_old = np.array([])
+
+        # counter for keeping track of the amount of coordinates so far.
+        self.count = 0
+
+    def stop(self):
+        """
+        Stop the app.
+
+        :return: Nothing
+        """
+        self.capLeft.stop()
+        self.capRight.stop()
+        cv.destroyAllWindows()
+
+    def start(self):
+        """
+        Main loop. Press q to quit.
+
+        :return: Nothing.
+        """
+
+        # optional counter for iterations per second
+        cps = CountsPerSec().start()
+
+        while True:
+
+            # Collect frames from the camera threads.
+            leftTimestamp, leftFrame = self.capLeft.getFrame()
+            rightTimestamp, rightFrame = self.capRight.getFrame()
+
+            # Flip the frames.
+            leftFrame = cv.flip(leftFrame, 0)
+            rightFrame = cv.flip(rightFrame, 0)
+
+            # Check if the images are too far apart in time.
+            diff = abs(leftTimestamp - rightTimestamp)
+            if diff > self.min_time_diff:
+                logging.info(f'Diff too big: {round(diff, 4)} seconds')
+                continue
+
+            # Check, if the new and the old images from the camera are the same.
+            if self.left_frame_old.shape[0] == 0 or self.right_frame_old.shape[0] == 0:
+                self.left_frame_old = leftFrame
+                self.right_frame_old = rightFrame
+            elif np.array_equal(leftFrame, self.left_frame_old) or np.array_equal(rightFrame, self.right_frame_old):
+                logging.info('duplicate in one of the cams')
+                continue
+
+            # the pictures getting processed
+            leftFound, leftCenter, leftFrameMasked = self.process_frame(leftFrame)
+            rightFound, rightCenter, rightFrameMasked = self.process_frame(rightFrame)
+
+            # If there is no object in left or right frame, this text is getting put onto the frame.
+            if not leftFound or not rightFound:
+                cv.putText(rightFrameMasked, "No traceable object found", (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1.2,
+                           (0, 0, 255), 3)
+                cv.putText(leftFrameMasked, "No traceable object found", (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1.2,
+                           (0, 0, 255), 3)
+
+                # in the case that the tracking is lost, the counter and the array of coordinates is getting reset
+                self.count = 0
+                self.positions = []
+
+            else:
+                self.count = self.count + 1
+                # calculate the distance between the object and the baseline of the cameras [cm]
+                depth = self.tr.find_depth(right_point=rightCenter,
+                                           left_point=leftCenter,
+                                           frame_right=rightFrameMasked,
+                                           frame_left=leftFrameMasked,
+                                           baseline=self.baseline,
+                                           f=self.focal_length,
+                                           alpha=self.fov)
+                # the coordinates are put into an array
+                position = (leftCenter[0], leftCenter[1], int(depth))
+
+                # if the array is empty, the current coordinate is put as the first entry.
+                self.positions.append(position)
+                print(len(self.positions))
+
+                # determine if already enough coordinates were collected
+                if self.count > self.min_samples:
+
+                    # return an array, containing predicted  n (20) coordinates.
+                    predicted_path = self.tp.predict_path(np.asarray(self.positions), 20)
+                    leftFrame = self.draw_points_to_frame(predicted_path, leftFrame)
+
+                # since a depth has been calculated, it will be shown in the frame
+                cv.putText(rightFrameMasked, "Distance: " + str(round(depth, 1)), (50, 50), cv.FONT_HERSHEY_SIMPLEX,
+                           1.2,
+                           (0, 255, 0), 3)
+                cv.putText(leftFrameMasked, "Distance: " + str(round(depth, 1)), (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1.2,
+                           (0, 255, 0), 3)
+                logging.info(self.positions)
+
+            # the counter of iterations per second is put on to the frame as well
+            cps.increment()
+            leftFrame = self.put_iterations_per_sec(leftFrame, cps.countsPerSec())
+
+            # the left and right frame is stacked together for better view.
+            frames = np.hstack((leftFrame, rightFrame))
+            cv.imshow("Frames", frames)
+
+            # Hit "q" to close the window
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                logging.info("Saving parameters!")
+                self.stop()
+                break
+
+    def put_iterations_per_sec(self, frame, iterations_per_sec):
+        """
+        Put the Iterations per Second Number on to a frame.
+
+        :param frame: as np.array
+        :param iterations_per_sec: number
+        :return: frame with number
+        """
+
+        cv.putText(frame, "{:.0f} iterations/sec".format(iterations_per_sec),
+                   (10, 650), cv.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255))
+        return frame
+
+    def process_frame(self, frame):
+        """
+        Apply filter and object detection on frame.
+        If no Object is found, the boolean found is False and the center is None.
+        The frame is returned in any case.
+
+        :param frame: as np.array
+        :return: center found or not as boolean, coordinates as array , maskedframe as np.array
+        """
+
+        frameMasked = self.fltr.apply_filter(frame)
+        found, center, frameMasked = self.od.detect_object(frame, frameMasked)
+        return found, center, frameMasked
+
+    def draw_points_to_frame(self, points: np.array, frame) -> np.array:
+        """
+        Get points and draws them onto the frame.
+
+        :param points: Array of 3D Points, ony x and y are used
+        :param frame: as np.array
+        :return: the frame.
+        """
+        for pt in points:
+            x, y = pt[0], pt[1]
+            if 0 <= x <= frame.shape[1] and 0 <= y <= frame.shape[0]:
+                cv.circle(frame, (int(x), int(y)), 2, (255, 0, 0), 10)
+        return frame
 
 
-def process_frame(frame):
-    """
-    Apply filter and object detection on frame.
-    If no Object is found, the boolean found is False and the center is None.
-    The frame is returned in any case.
-
-    :param frame: as np.array
-    :return: center found or not as boolean, coordinates as array , maskedframe as np.array
-    """
-
-    frameMasked = fltr.apply_filter(frame)
-    found, center, frameMasked = od.detect_object(frame, frameMasked)
-    return found, center, frameMasked
-
-
-def draw_points_to_frame(points: np.array, frame) -> np.array:
-    """
-    Get points and draws them onto the frame.
-
-    :param points: Array of 3D Points, ony x and y are used
-    :param frame: as np.array
-    :return: the frame.
-    """
-    for pt in points:
-        x, y = pt[0], pt[1]
-        if 0 <= x <= frame.shape[1] and 0 <= y <= frame.shape[0]:
-            cv.circle(frame, (int(x), int(y)), 2, (255, 0, 0), 10)
-    return frame
-
-
-# optional counter for iterations per second
-cps = CountsPerSec().start()
-
-# Main loop
-while True:
-
-    # Collect frames from the camera threads.
-    leftTimestamp, leftFrame = capLeft.getFrame()
-    rightTimestamp, rightFrame = capRight.getFrame()
-
-    # Flip the frames.
-    leftFrame = cv.flip(leftFrame, 0)
-    rightFrame = cv.flip(rightFrame, 0)
-
-    # Check if the images are too far apart in time.
-    diff = abs(leftTimestamp - rightTimestamp)
-    if diff > min_time_diff:
-        logging.info(f'Diff too big: {round(diff, 4)} seconds')
-        continue
-
-    # Check, if the new and the old images from the camera are the same.
-    if left_frame_old.shape[0] == 0 or right_frame_old.shape[0] == 0:
-        left_frame_old = leftFrame
-        right_frame_old = rightFrame
-    elif np.array_equal(leftFrame, left_frame_old) or np.array_equal(rightFrame, right_frame_old):
-        logging.info('duplicate in one of the cams')
-        continue
-
-    # the pictures getting processed
-    leftFound, leftCenter, leftFrameMasked = process_frame(leftFrame)
-    rightFound, rightCenter, rightFrameMasked = process_frame(rightFrame)
-
-    # If there is no object in left or right frame, this text is getting put onto the frame.
-    if not leftFound or not rightFound:
-        cv.putText(rightFrameMasked, "No traceable object found", (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1.2,
-                   (0, 0, 255), 3)
-        cv.putText(leftFrameMasked, "No traceable object found", (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1.2,
-                   (0, 0, 255), 3)
-
-        # in the case that the tracking is lost, the counter and the array of coordinates is getting reset
-        count = 0
-        positions = []
-
-    else:
-        count = count + 1
-        # calculate the distance between the object and the baseline of the cameras [cm]
-        depth = tr.find_depth(right_point=rightCenter,
-                               left_point=leftCenter,
-                               frame_right=rightFrameMasked,
-                               frame_left=leftFrameMasked,
-                               baseline=B,
-                               f=f,
-                               alpha=alpha)
-        # the coordinates are put into an array
-        position = (leftCenter[0], leftCenter[1], int(depth))
-
-        # if the array is empty, the current coordinate is put as the first entry.
-        positions.append(position)
-
-        # determine if already enough coordinates were collected
-        if count > min_samples:
-            # return an array, containing predicted  n (20) coordinates.
-            predicted_path = tp.predict_path(np.asarray(positions), 20)
-            leftFrame = draw_points_to_frame(predicted_path, leftFrame)
-
-        # since a depth has been calculated, it will be shown in the frame
-        cv.putText(rightFrameMasked, "Distance: " + str(round(depth, 1)), (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1.2,
-                   (0, 255, 0), 3)
-        cv.putText(leftFrameMasked, "Distance: " + str(round(depth, 1)), (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1.2,
-                   (0, 255, 0), 3)
-        logging.info(positions)
-
-    # the counter of iterations per second is put on to the frame as well
-    cps.increment()
-    leftFrame = put_iterations_per_sec(leftFrame, cps.countsPerSec())
-
-    # the left and right frame is stacked together for better view.
-    frames = np.hstack((leftFrame, rightFrame))
-    cv.imshow("Frames", frames)
-
-    # Hit "q" to close the window
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        logging.info("Saving parameters!")
-        file = open("params.txt", "w")
-        for corrd in positions:
-            file.write(str(corrd[0]) + "," + str(corrd[1]) + "," + str(corrd[2]) + "\n")
-        file.close()
-        capLeft.stop()
-        capRight.stop()
-        break
-
-cv.destroyAllWindows()
+if __name__ == "__main__":
+    app = Application()
+    app.start()
---
+++ StereoVision/ur5_03.py
@@ -0,0 +1,248 @@
+# ur5_3.py: Echo client program to send commands via socket to robot
+# see: https://www.zacobria.com/universal-robots-knowledge-base-tech-support-forum-hints-tips/universal-robots-script-programming/
+# neu in V2: Ansteuerung mit "korrektem" x-Abschnitt, mit conf-Datei für Parameter
+# neu in V3: interne Transformation von Pixel-Koord (0...width [Pixel]) nach Robot-Coords ( [m])
+#
+import socket  # for sending commands to robot
+import time  # for waiting some time for robot to complete move
+import commentjson  # for parsing config-file with parameters
+import argparse  # for parsing command line argument
+from enum import Enum  # enumerate some predefined positions
+import numpy as np  # math
+
+# see config file: HOST = "127.0.0.1" # The remote host
+# see config file: PORT = 30002 # The same port as used by the server
+sloMo = 0.9  # slow motion factor: 0.2 ... 1.0 ... 2.0
+t1 = 0.9 * sloMo  # time constants [s] to wait for robot to complete
+t2 = 0.5 * sloMo
+t3 = 0.9  # time before next command
+
+
+class Position(Enum):
+    # poses in Standard orientation, i.e. +X-axis pointing forward
+    right = 0
+    middle = 1
+    left = 2
+    # poses in Robo-Lab, i.e. -Y-axis pointing forward
+    rightRL = 3
+    middleRL = 4
+    leftRL = 5
+
+
+class urControl():
+    # main class to control the URx-robot
+    def __init__(self, conf):
+        # define some poses 
+        # conf: parameters from config file
+        self.ToolPoses = [[0.45, -0.70, 0.05, 0.0, 1.57, 0],  # right
+                          [0.45, 0.0, 0.05, 0.0, 1.57, 0],  # neutral/mid
+                          [0.45, 0.70, 0.05, 0.0, 1.57, 0]  # left
+                          ]
+        self.JointPoses = [
+            # regular Orientation
+            [1.864, -0.307, 0.675, -0.371, 0.293, -1.569],  # right
+            [2.967, -1.102, 2.704, -1.603, 1.396, -1.571],  # middle
+            [3.806, -0.161, 0.371, -0.214, 2.235, -1.571],  # left
+            # Robo-Lab Orientation, i.e. -Y pointing forward
+            [0.279, -0.307, 0.675, -0.371, 0.293, -1.571],  # right RL
+            [1.172, -1.041, 2.358, -1.317, 1.187, -1.571],  # middle RL
+            [2.381, -0.63, 1.314, -0.737, 2.395, -1.575]  # left RL
+        ]
+        # get parameters from config file:
+        self.xMin = conf["robotXmin"]
+        self.xMax = conf["robotXmax"]
+        self.yMin = conf["robotYmin"]
+        self.yMax = conf["robotYmax"]
+        self.frameWidth = conf["frameWidth"]  # width of Video frame in Pixels
+        self.frameDepth = conf["frameDepth"]  # Depth of ball Position in Pixels
+        self.atransMat = None  # matrix for affine transformation to Robot coord system
+        self.getTransformation()  # calculate transformation matrix
+        self.HOST = conf["robotHOST"]
+        self.PORT = conf["robotPORT"]
+
+        # open socket to robot
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        while True:
+            try:
+                self.s.connect((self.HOST, self.PORT))
+                print("Socket connected")
+                # self.startPos()
+                break
+            except OSError as msg:
+                self.s.close()
+                print("Socket open failed: %pts1= s" % msg)
+                raise EnvironmentError('No connection to Robot')
+
+    def close(self):
+        # close socket when program exits
+        data = self.s.recv(1024)
+        print("Received", repr(data))
+        self.s.close()
+        print("Socket closed")
+
+    def getTransformation(self):
+        # calculate matrix for affine Transformation
+        # to transform pixel coords into global Robot coord system
+        # can use OpenCV to get the Matrix
+        # see: https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_geometric_transformations/py_geometric_transformations.html
+        # self.atransMat= cv2.getAffineTransform(ptsOrg, ptsRobot)
+
+        # do it manually to avoid dependency on OpenCV at this point
+        self.atransMat = np.array(
+            [[(self.xMax - self.xMin) / self.frameWidth, 0, self.xMin],
+             [0, (self.yMin - self.yMax) / self.frameDepth, self.yMax]]
+            # Example: [[ 0.0025,      0.0,     -0.6],
+            # [0.0,              -0.02,      -0.2]]
+        )
+        print("Matrix= ", self.atransMat)
+
+    def startPos(self):
+        print("Robot moves into default pose")
+        # start "middle" pos
+        # self.s.send (("movel([  0.0, -1.745,-2.217, -2.199, -1.623,  0.0 ], a=1.0, v=2.1, t=0.5)" + "\n").encode())
+        # time.sleep(t1*2)
+        self.move2JointPose(Position.right)
+        time.sleep(t1 * 3)
+        # self.move2JointPose(Position.rightRL)     # right (Robo-Lab)
+        # time.sleep(t1*3)
+        self.move2JointPose(Position.middleRL)
+        time.sleep(t1 * 2)
+
+    def move2ToolPose(self, pose):
+        # linear movement using x,y,z,rx,ry,rz-Coordinates
+        print("Move to Tool-Pose ", pose.name)
+        cmd = "movel(p" + str(self.ToolPoses[pose.value]) + ", a=2200, v=250)" + "\n"
+        print("Cmd: ", cmd)
+        self.s.send(cmd.encode())
+
+    def move2JointPose(self, pose):
+        # joint (angular) movement to predefined Joint-Pose using joint-angles in RAD!!
+        print("Move to Joint-Pose ", pose.name)
+        sloMoCmd = ", a={:f}, v={:f})".format(2200.0 / sloMo, 180.0 / sloMo)
+        cmd = "movej(" + str(self.JointPoses[pose.value]) + sloMoCmd + "\n"
+        print("Cmd: ", cmd)
+        self.s.send(cmd.encode())
+
+    def move2ToolYCoord(self, yCoord):
+        # move to Coord [0.4, yCoord, 0.05], used in standard orientation of Robot
+        # print("movde2ToolYCoord: y=", yCoord)
+        self.move2ToolCoord([0.4, yCoord])
+
+    def move2ToolCoord(self, position):
+        # linear move to predefined tool-Pose (i.e. using x,y,z,rx,ry,rz-Coordinates)
+        print("Move to Tool-Coord: ", position)
+        if position is None:
+            return None
+        if position[0] < self.xMin:
+            position[0] = self.xMin
+        elif position[0] > self.xMax:
+            position[0] = self.xMax
+        if position[1] < self.yMin:
+            position[1] = self.yMin
+        elif position[1] > self.yMax:
+            position[1] = self.yMax
+        pose = [position[0], position[1], 0.05, 0.0, 1.57, 0]
+        sloMoCmd = ", a={:f}, v={:f})".format(2200.0 / sloMo, 120.1 / sloMo)
+        cmd = "movel(p" + str(pose) + sloMoCmd + "\n"
+        print("Cmd: ", cmd)
+        self.s.send(cmd.encode())
+
+    def move2CamCoord(self, pixCoord):
+        # linear move to transformed Camera-Coords [x-Pixel, "depth-Pixel"]
+        print("Move to Cam-Coord with aff. Transformation: ", pixCoord)
+        if pixCoord is None:
+            return None
+        if pixCoord[0] < 0:
+            pixCoord[0] = 0;
+        elif pixCoord[0] > self.frameWidth:
+            pixCoord[0] = self.frameWidth
+        if pixCoord[1] < 0:
+            pixCoord[1] = 0;
+        elif pixCoord[1] > 20:
+            pixCoord[1] = 20
+        pixCoord3 = np.array([[pixCoord[0]], [pixCoord[1]], [1]])
+        # result= M*pixCood*(transpose)
+        robotCoord = self.atransMat.dot(pixCoord3).T
+        print("RobotCoord= ", robotCoord)
+        # pose= [ robotCoord[0,0], robotCoord[0,1], 0.05, 1.016, 1.321, -1.020]
+        # set z-Coord
+        pose = [robotCoord[0, 0], robotCoord[0, 1], 0.12, 1.016, 1.321, -1.020]
+        # sloMoCmd= ", a=2200.0, v=120.1)"
+        sloMoCmd = ", a={:f}, v={:f})".format(2200.0 / sloMo, 120.1 / sloMo)
+        cmd = "movel(p" + str(pose) + sloMoCmd + "\n"
+        print("Cmd: ", cmd)
+        self.s.send(cmd.encode())
+
+
+if __name__ == "__main__":
+    # main routine for testing, when started "stand-alone"
+    print("Starting Program Robot Test")
+    # get command-line arg, i.e. -c config-file 
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-c", "--conf", required=True,
+                    help="path to the JSON configuration file")
+    arguments = vars(ap.parse_args())
+    # get all parameters from config file
+    conf = commentjson.load(open(arguments["conf"]))
+
+    # check communication to robot (or robot-simulation)
+    try:
+        myUrControl = urControl(conf)  # includes moving to middle position
+    except EnvironmentError as msg:
+        print("Oops: %s" % msg)
+    else:
+        while True:
+            print('Main test loop')
+            # robot is alreade in  "middle" position using Robot-Lab orientation
+            # enter command (incl "return"-key)
+            userInput = input("Cmd [q, a, b, c, d, e, f, g]: ")
+            # not all commands work at all time,
+            # try: g --> move to middle position
+            # try: e --> move to right / middle / left position
+            # q: quit program
+            # print ('you entered', userInput)
+            if userInput == 'q':
+                myUrControl.close()
+                break;
+            elif userInput == 'a':
+                myUrControl.move2ToolCoord([0.5, 0.3])
+                time.sleep(t1 * 3)
+            elif userInput == 'b':
+                myUrControl.move2ToolCoord([0.65, 0.6])
+                time.sleep(t1 * 3)
+            elif userInput == 'c':
+                myUrControl.move2ToolCoord([0.3, 0.1])
+                time.sleep(t1 * 3)
+            elif userInput == 'd':
+                myUrControl.move2ToolPose(Position.left)
+                time.sleep(t1 * 2)
+                myUrControl.move2ToolPose(Position.right)
+                time.sleep(t1 * 2)
+                myUrControl.move2ToolPose(Position.middle)
+                time.sleep(t1)
+                myUrControl.move2ToolPose(Position.right)
+                time.sleep(t1 * 2)
+            elif userInput == 'e':
+                myUrControl.move2JointPose(Position.left)
+                time.sleep(t3)
+                myUrControl.move2JointPose(Position.right)
+                time.sleep(t3)
+                myUrControl.move2JointPose(Position.middle)
+                time.sleep(t3)
+                myUrControl.move2JointPose(Position.right)
+                time.sleep(t3)
+            elif userInput == 'f':
+                myUrControl.move2CamCoord([0, 0])
+                time.sleep(t3)
+                myUrControl.move2CamCoord([myUrControl.frameWidth / 2, myUrControl.frameDepth])
+                time.sleep(t3)
+                myUrControl.move2CamCoord([myUrControl.frameWidth, 0])
+                time.sleep(t3)
+                myUrControl.move2CamCoord([20, 20])
+                time.sleep(t3)
+            elif userInput == 'g':
+                myUrControl.move2JointPose(Position.middleRL)
+                time.sleep(t3)
+
+    print("Finished Program Robot Test")
+    exit()
